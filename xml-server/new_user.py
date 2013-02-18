6cb8ca945dd470c68058785fb23e831dcde6597e
bl0ckeduser <bl0ckedusersoft@gmail.com>
2013-02-17 19:52:10 -0500

    compatibilité mingw: suppression de os.system() dans xml-serv

--- xml-server/new_user.py
+++ xml-server/new_user.py
@@ -5,44 +5,47 @@ import os.path
 import sys
 import urlparse
 
-dat = sys.argv[1]
+def do_new_user(dat):
+	fields = urlparse.parse_qs(dat)
 
-fields = urlparse.parse_qs(dat)
+	username = fields['username'][0]
+	passwd = fields['password'][0]
+	t = fields['tete'][0]
+	c = fields['corps'][0]
+	p = fields['pied'][0]
+	tc = fields['tetecouleur'][0]
+	cc = fields['corpscouleur'][0]
+	pc = fields['piedcouleur'][0]
+	try:
+		desc = fields['description'][0]
+	except KeyError:
+		desc = ""
+	
+	try:
+		comm = fields['commentaire'][0]
+	except KeyError:
+		comm = ""
+	
+	if not os.path.isfile("users/" + username):
+		# user not registed yet; good !
+		data = open("users/" + username, "w")
+		data.write(passwd + '\n')
+		data.write(tc + '\n')
+		data.write(cc + '\n')
+		data.write(pc + '\n')
+		data.write(desc + '\n')
+		data.write(comm + '\n')
+		data.write(c + '\n')
+		data.write(t + '\n')
+		data.write(p + '\n')
+		data.close()
 
-username = fields['username'][0]
-passwd = fields['password'][0]
-t = fields['tete'][0]
-c = fields['corps'][0]
-p = fields['pied'][0]
-tc = fields['tetecouleur'][0]
-cc = fields['corpscouleur'][0]
-pc = fields['piedcouleur'][0]
-try:
-	desc = fields['description'][0]
-except KeyError:
-	desc = ""
+		# pas trouve mieux pour que ca
+		# marche sur presque toutes les versions :(
+		print " &erreur=Inscription reussie& &resultat=0& "
+	else:
+		print " &erreur=Usager existe deja& &resultat=0& "
 
-try:
-	comm = fields['commentaire'][0]
-except KeyError:
-	comm = ""
-
-if not os.path.isfile("users/" + username):
-	# user not registed yet; good !
-	data = open("users/" + username, "w")
-	data.write(passwd + '\n')
-	data.write(tc + '\n')
-	data.write(cc + '\n')
-	data.write(pc + '\n')
-	data.write(desc + '\n')
-	data.write(comm + '\n')
-	data.write(c + '\n')
-	data.write(t + '\n')
-	data.write(p + '\n')
-	data.close()
-
-	# pas trouve mieux pour que ca
-	# marche sur presque toutes les versions :(
-	print " &erreur=Inscription reussie& &resultat=0& "
-else:
-	print " &erreur=Usager existe deja& &resultat=0& "
+# fichier seul
+if __name__ == "__main__":	
+	do_new_user(sys.argv[1])
--- xml-server/update_user.py
+++ xml-server/update_user.py
@@ -5,46 +5,50 @@ import os
 import sys
 import urlparse
 
-dat = sys.argv[1]
+def do_update_user(dat):
+	fields = urlparse.parse_qs(dat)
 
-fields = urlparse.parse_qs(dat)
+	username = fields['username'][0]
+	t = fields['tete'][0]
+	c = fields['corps'][0]
+	p = fields['pied'][0]
+	tc = fields['tetecouleur'][0]
+	cc = fields['corpscouleur'][0]
+	pc = fields['piedcouleur'][0]
+	try:
+		desc = fields['description'][0]
+	except KeyError:
+		desc = ""
+	
+	try:
+		comm = fields['commentaire'][0]
+	except KeyError:
+		comm = ""
+	
+	if 1:
+		data = open("users/" + username, "r")
+		passwd = data.readline().strip()
+		data.close()
+	
+		os.remove("users/" + username)
+	
+		data = open("users/" + username, "w")
+		data.write(passwd + '\n')
+		data.write(tc + '\n')
+		data.write(cc + '\n')
+		data.write(pc + '\n')
+		data.write(desc + '\n')
+		data.write(comm + '\n')
+		data.write(c + '\n')
+		data.write(t + '\n')
+		data.write(p + '\n')
+		data.close()
 
-username = fields['username'][0]
-t = fields['tete'][0]
-c = fields['corps'][0]
-p = fields['pied'][0]
-tc = fields['tetecouleur'][0]
-cc = fields['corpscouleur'][0]
-pc = fields['piedcouleur'][0]
-try:
-	desc = fields['description'][0]
-except KeyError:
-	desc = ""
+		print " "
+		print dat+" &resultat=1& &success=1&"
+		print ""
+	
+# fichier seul
+if __name__ == "__main__":
+	do_update_user(sys.argv[1])
 
-try:
-	comm = fields['commentaire'][0]
-except KeyError:
-	comm = ""
-
-if 1:
-	data = open("users/" + username, "r")
-	passwd = data.readline().strip()
-	data.close()
-
-	os.remove("users/" + username)
-
-	data = open("users/" + username, "w")
-	data.write(passwd + '\n')
-	data.write(tc + '\n')
-	data.write(cc + '\n')
-	data.write(pc + '\n')
-	data.write(desc + '\n')
-	data.write(comm + '\n')
-	data.write(c + '\n')
-	data.write(t + '\n')
-	data.write(p + '\n')
-	data.close()
-
-	print " "
-	print dat+" &resultat=1& &success=1&"
-	print ""
