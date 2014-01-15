2b40a6787c7ab7ed368e01f925b9314b56e25c5e
bl0ckeduser <bl0ckedusersoft@gmail.com>
2014-01-14 21:46:05 -0500

    Ajout de notes sur la création des archives finales

--- finaliser.sh
+++ finaliser.sh
@@ -2,3 +2,14 @@ mkdir fichiers-client
 cd xml-server
 mkdir amis
 mkdir stockage_messagiel
+
+# Instructions de finalisation
+# (passer du GIT aux archives finales unix et windows)
+
+# Pour finaliser la distribution linux:
+# faire rouler ce script puis l'enlever
+
+
+# Pour meta-finaliser l'archive windows: enlever finaliser.sh
+# et compilation_exe (d'abord il faut compiler et finaliser,
+# voir dans compilation_exe. ca prend py2exe et mingw et ...)
